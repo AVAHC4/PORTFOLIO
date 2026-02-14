44dfc4cfdb5d7841c55f57a01bf427eef19d7f8e
AVAHC4 <akhilchava4@gmail.com>
2026-02-14 21:34:54 +0530

    update resume link and resend key

--- my-portfolio/src/config/Resume.ts
+++ my-portfolio/src/config/Resume.ts
@@ -1,3 +1,3 @@
 export const resumeConfig = {
-  url: 'https://drive.google.com/file/d/1ormIiMVpWGAMOZ3FZVj_XrKPkEmPlPQj/preview',
+  url: 'https://drive.google.com/file/d/1obHG2kebohLjVBbC0wgc74h097pNdpmj/view?usp=sharing',
 };
